5f340e75857e530f0fb7c17710a4935637e43782
Emma Weberyd <emma.weberyd@gmail.com>
2023-12-03 20:54:23 +0100

    refactor day 3 part 1

--- 3/part1.py
+++ 3/part1.py
@@ -1,4 +1,5 @@
 import re
+from utils import *
 
 def find_numbers(line):
     return [(int(match.group(0)),int(match.start(0)), int(match.end(0))) for match in re.finditer(r"\d+", line)]
@@ -6,21 +7,19 @@ def find_numbers(line):
 def is_part_number(search_area):
     return bool(re.search(r"[^\d.]", search_area))
 
+def find_trbl_search_area(line_number, line, lines, number):
+    return max(line_number - 1, 0), min(number[2] + 1, len(line) - 1), min(line_number + 1, len(lines) - 1) + 1, max(number[1] - 1, 0)
 
 def sum_of_part_numbers(input_file):
-    with open(input_file) as f:
-        lines = f.read().splitlines()
+    lines = read_input(input_file)
 
     sum = 0
 
     for line_number, line in enumerate(lines):
         numbers = find_numbers(line)
         for number in numbers:
-            left_limit = max(number[1] - 1, 0)
-            right_limit = min(number[2] + 1, len(line) - 1)
-            top_limit = max(line_number - 1, 0)
-            bottom_limit = min(line_number + 1, len(lines) - 1) + 1
-            search_area = ''.join([line[left_limit:right_limit] for line in lines[top_limit:bottom_limit]])
+            t, r, b, l = find_trbl_search_area(line_number, line, lines, number)
+            search_area = ''.join([line[l:r] for line in lines[t:b]])
             if is_part_number(search_area): sum += number[0]
     return sum
 
