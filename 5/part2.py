c365ffc914466da0d06074b5f635e8707072dae6
Emma Weberyd <emma.weberyd@gmail.com>
2023-12-06 22:53:17 +0100

    day 5 part 2

--- 5/part2.py
+++ 5/part2.py
@@ -3,6 +3,17 @@ from part1 import convert, find_numbers
 import numpy as np
 import re
 
+INF = 2**63 - 1
+
+def intersection(source_range, map_range):
+    source_start, source_end = source_range
+    destination_start, map_start, map_end = map_range
+
+    if source_start <= map_end and source_end >= map_start:
+        offset = (destination_start-map_start)
+        return np.array([max(source_start, map_start) + offset, min(source_end, map_end)  + offset])
+    return np.array([])
+
 def get_almanac_from_file(file_name):
     with open(file_name, 'r') as file:
         data = file.read()
@@ -16,22 +27,49 @@ def get_almanac_from_file(file_name):
             conversion_map.append(find_numbers(piece))
         almanac.append(conversion_map)
     
-    source_map = np.reshape(np.array(almanac[0][0]), (-1,2)).astype(int)
-    sources = []
-    for source in source_map:
-        sources += [*range(source[0],source[0]+source[1])]
-    
-    return sources, almanac[1:]
+    return np.reshape(np.array(almanac[0][0]), (-1,2)).astype(int), almanac[1:]
+
+
+def add_missing_ranges(almanac): 
+    new_almanac = []
+    for i, conversion_map in enumerate(almanac):
+        conversion_map.sort(key=lambda x: x[1])
+
+        new_conversion_map = []
+        if conversion_map[0][1] != 0:
+            new_conversion_map.append([0, 0, conversion_map[0][1]-1])
+
+        for j, map_piece in enumerate(conversion_map):
+            if j+1 == len(conversion_map):
+                new_conversion_map.append([map_piece[2]+1, map_piece[2]+1, INF])
+            elif map_piece[2]+1 != conversion_map[j+1][1]:
+                new_conversion_map.append([map_piece[2]+1, map_piece[2]+1, conversion_map[j+1][1]-1])
+                
+        new_almanac.append(new_conversion_map + conversion_map)
+    return new_almanac
 
 def find_lowest_location_numner(file_name):
     sources, almanac = get_almanac_from_file(file_name)
 
-    for part in almanac:
-        for i, source in enumerate(sources):
-            next_source = convert(source, part)
-            sources[i] = next_source
+    sources = [[source_start, source_start+source_count-1] for source_start, source_count in sources]
 
-    return min(sources)
+    alm = []
+    for conversion_map in almanac:
+        alm.append([[int(destination_start), int(source_start), int(source_start)+int(count)-1] for (destination_start, source_start, count) in conversion_map])
+
+    almanac = add_missing_ranges(alm)
+    
+    for conversion_map in almanac:
+        new_sources = []
+        for source_range in sources:
+            for map_piece in conversion_map:
+                intersect = intersection(source_range, map_piece)
+                if intersect.shape != (0,): 
+                    new_sources.append(intersect) 
+        sources = new_sources
+    
+    return min(np.array(sources).flatten())
+
 
 if __name__ == "__main__":
-    print(find_lowest_location_numner("test_input.txt"))
+    print(find_lowest_location_numner("input.txt"))
--- 5/test2.py
+++ 5/test2.py
@@ -1,10 +1,32 @@
 import unittest
 from part2 import *
+import numpy as np
+import sys
+
+INF = 2**63 - 1
 
 class TestSolution(unittest.TestCase):
 
     def test_find_lowest_location_numner(self):
         self.assertEqual(find_lowest_location_numner('test_input.txt'), 46, "Should be 46")
+    
+    def test_add_missing_ranges(self):
+        self.assertEqual(add_missing_ranges([[[50, 98, 99], [52, 50, 97]]]), [[[0, 0, 49], [100, 100, INF], [52, 50, 97], [50, 98, 99]]])
+        self.assertEqual(add_missing_ranges([[[50, 98, 99], [52, 50, 96]]]), [[[0, 0, 49], [97, 97, 97], [100, 100, INF], [52, 50, 96], [50, 98, 99]]])
+        self.assertEqual(add_missing_ranges([[[50, 98, 99], [52, 50, 96]], [[50, 98, 99], [52, 50, 97]]]), [[[0, 0, 49], [97, 97, 97], [100, 100, INF], [52, 50, 96], [50, 98, 99]], [[0, 0, 49], [100, 100, INF], [52, 50, 97], [50, 98, 99]]])
+
+    def test_intersection(self):
+        self.assertEqual((intersection(np.array([79, 92]), np.array([50, 98, 99]))==np.array([])).all(), True)
+        self.assertEqual((intersection(np.array([79, 92]), np.array([52, 50, 97]))==np.array([81, 94])).all(), True)
+        self.assertEqual((intersection(np.array([55, 67]), np.array([50, 98, 99]))==np.array([])).all(), True)
+        self.assertEqual((intersection(np.array([55, 67]), np.array([52, 50, 97]))==np.array([57, 69])).all(), True)
+        self.assertEqual((intersection(np.array([0, 3]), np.array([0, 2, 48]))==np.array([0, 1])).all(), True)
+        self.assertEqual((intersection(np.array([0, 3]), np.array([0, 2, INF]))==np.array([0, 1])).all(), True)
+        self.assertEqual((intersection(np.array([20, 80]), np.array([0, 2, 19]))==np.array([])).all(), True)
+        self.assertEqual((intersection(np.array([20, 80]), np.array([0, 2, 19]))==np.array([])).all(), True)
+        self.assertEqual((intersection(np.array([8974539875347, 8974539875349]), np.array([8974539875348, 8974539875348, INF]))==np.array([8974539875348, 8974539875349])).all(), True)
+
+        
 
 
 if __name__ == '__main__':
