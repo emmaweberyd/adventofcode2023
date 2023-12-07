68f8db2fd7929b588e82d9a1bf94d415de921b3e
Emma Weberyd <emma.weberyd@gmail.com>
2023-12-07 22:42:33 +0100

    day 4 part 2

--- 4/part2.py
+++ 4/part2.py
@@ -2,29 +2,39 @@ from utils import *
 import re
 from part1 import listify_numbers, get_number_of_wins
 
-def extract_wins(card, cards):
-    parts = re.split('\||:', card)
-    winning_numbers, numbers = [set(listify_numbers(part)) for part in parts[1:]]
-    key, wins = int(listify_numbers(parts[0])[0]), len(numbers.intersection(winning_numbers))
-    won_cards = cards[key:min(key+wins, len(cards) - 1)]
-    print(key, won_cards)
-
-    return won_cards
-
-def count_wins(cards, all_cards):
-    if cards == []: return 0
-
-    sum = 0
-    for card in cards:
-        wins = extract_wins(card, all_cards)
-        sum += len(wins) + count_wins(wins, all_cards)
-    return sum
-
 def final_scratchcard_count(input_file):
     cards = read_input(input_file)
-    return len(cards) + count_wins(cards, cards)
+    card_holder = {}
+    sum = 0
+
+    for card in cards:
+        card_nr, winning_numbers, numbers = parse_card(card)
+        nr_of_wins = len(numbers.intersection(winning_numbers))
+
+        card_instances = 1
+        if card_nr in card_holder:
+            card_instances += card_holder[card_nr]
+
+        for win in range(card_nr+1, card_nr+1+nr_of_wins):
+            if win in card_holder:
+                card_holder[win] += card_instances
+            else:
+                card_holder[win] = card_instances
+              
+        sum += card_instances
+    return sum
+
+def find_numbers(line):
+    return [int(n) for n in re.findall(r'\d+', line)]
+
+def parse_card(card):
+    parts = re.split('\||:', card)
+    card_nr, winning_numbers, numbers = [find_numbers(part) for part in parts]
+    return card_nr[0], set(winning_numbers), set(numbers)
 
 if __name__ == "__main__":
     print(final_scratchcard_count('input.txt'))
 
+ 
+    
 
